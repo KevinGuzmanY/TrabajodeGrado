0f08506441ca082e6e82b0a941811af8b0d52346
KevinGuzmanY <82846196+KevinGuzmanY@users.noreply.github.com>
2024-09-15 13:18:17 -0500

    first

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api/api.service';
+import { delay } from 'rxjs/operators';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  moviesSlider: any[] = [];
+  tvSlider: any[] = [];
+  movies_data: any[] = [];
+  filteredMovies: any[] = [];
+  genres: any[] = [];
+
+  constructor(private apiService: ApiService, private spinner: NgxSpinnerService) { }
+
+  ngOnInit() {
+    this.spinner.show();
+    this.fetchTrendingContent('movie', 1, 'movies');
+    this.fetchTrendingContent('tv', 1, 'tvShows');
+    this.getNowPlaying('movie', 1);
+    this.getGenres();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 2000);
+  }
+
+  // Get Genres from the API
+  getGenres() {
+    this.apiService.getGenres('movie').subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.genres = res.genres;
+      },
+      error: (error) => {
+        console.error('Error fetching genres', error);
+      },
+      complete: () => {
+        console.log('Genres fetching complete');
+      }
+    });
+  }
+
+  // Handle Genre Selection
+  onGenreSelected(genreId: number) {
+    console.log('test')
+    this.filteredMovies = this.moviesSlider.filter(movie => movie.genre_ids.includes(genreId));
+  }
+
+  // Slider Data
+  getNowPlaying(mediaType: 'movie', page: number) {
+  this.apiService.getNowPlaying(mediaType, page).pipe(delay(2000)).subscribe(
+    (res: any) => {
+      this.movies_data = res.results.map((item: any) => {
+        const movieItem = {
+          ...item,
+          link: `/movie/${item.id}`,
+          videoId: '' // Initialize with an empty string
+        };
+
+        // Fetch the trailer video key for each movie
+        this.apiService.getYouTubeVideo(item.id, 'movie').subscribe(
+          (videoRes: any) => {
+            const video = videoRes.results.find((vid: any) => vid.site === 'YouTube' && vid.type === 'Trailer');
+            if (video) {
+              movieItem.videoId = video.key; // Set the video key if available
+            }
+          },
+          videoError => {
+            console.error('Error fetching YouTube video for Movie:', videoError);
+          }
+        );
+
+        return movieItem;
+      });
+    },
+    error => {
+      console.error('Error fetching now playing data', error);
+    }
+  );
+}
+  performSearch() {
+    // Aquí puedes agregar la lógica para realizar la búsqueda
+    alert('Buscar...');
+  }
+  fetchTrendingContent(media: string, page: number, type: string): void {
+    this.apiService.getTrending(media, page).subscribe(
+      response => {
+        if (type === 'movies') {
+          this.moviesSlider = response.results.map((item: any) => ({
+            link: `/movie/${item.id}`,
+            imgSrc: item.poster_path ? `https://image.tmdb.org/t/p/w370_and_h556_bestv2${item.poster_path}` : null,
+            title: item.title,
+            rating: item.vote_average * 10,
+            vote: item.vote_average
+          }));
+        } else if (type === 'tvShows') {
+          this.tvSlider = response.results.map((item: any) => ({
+            link: `/tv/${item.id}`,
+            imgSrc: item.poster_path ? `https://image.tmdb.org/t/p/w370_and_h556_bestv2${item.poster_path}` : null,
+            title: item.name,
+            rating: item.vote_average * 10,
+            vote: item.vote_average
+          }));
+        }
+      },
+      error => {
+        console.error(`Error fetching trending ${type}:`, error);
+      }
+    );
+  }
+
+}
